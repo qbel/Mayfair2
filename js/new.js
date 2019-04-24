d39bc0344873a35529b34953a3691be1231724db
qbel <queenjummy0@gmail.com>
2019-04-24 17:11:53 +0300

    Latest Commit

--- js/new.js
+++ js/new.js
@@ -133,3 +133,8 @@ function myFunction() {
 // }
 
 
+$('ul.nav li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+});
